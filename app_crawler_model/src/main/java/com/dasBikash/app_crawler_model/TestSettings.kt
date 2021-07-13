c9566e3ba4617a70785497bf828c3541712d51f1
Bikash Das <das.bikash.dev@gmail.com>
2021-07-13 18:46:01 +0600

    documentation added for .

--- app_crawler_model/src/main/java/com/dasBikash/app_crawler_model/TestSettings.kt
+++ app_crawler_model/src/main/java/com/dasBikash/app_crawler_model/TestSettings.kt
@@ -4,14 +4,43 @@ import androidx.annotation.Keep
 
 
 /**
- * specify values for edit text by resource id.
- * specify edit texts that are not to be touched.
- * Default resource id to click on pop up.
- * Specify whether to enable curtion or not.
- * move url filter to config.
- * move current params (path,max time, onlyScript) to config.
- * specify whether to use personal app space for reports, default will be cache.
-* */
+ * Class to provide test settings during test launch
+ *
+ * @property testScriptPath `Remote/local(Private app storage)/assets` script path, if null given then
+ *                          auto test will run
+ * @property useFilesDirForReport for true report files will be saved in `files` directory else in `cache
+ *                              directory of respective app's private storage
+ * @property enableClickBlocker if true all touch events will be blocked during test
+ *
+ * @property maxRunTimeMinutes Maximum test run time in minutes (>=30 will be truncated to 30)
+ *
+ * @property runOnlyScript whether to run only script or run unscripted robo test
+ *                          also, at the end of scripted action execution.
+ *
+ * @property requestMethodFilter optional RequestMethodFilter instance to restrict specific api calls
+ *  [see also](https://github.com/dasBikash84/app-crawler-model/blob/master/app_crawler_model/src/main/java/com/dasBikash/app_crawler_model/RequestMethodFilter.kt)
+ *
+ * - To allow only `GET` api calls => `RequestMethodFilter.methodGetFilter()`
+ * - To allow only `GET` and any other api containing '/login' in path =>
+ *      RequestMethodFilter.methodGetFilter(listOf("/login"))
+ * - To allow only `GET` and `POST` api calls => RequestMethodFilter.methodGetPostFilter()
+ * - To allow only `GET` , `POST` and `DELETE` api calls => RequestMethodFilter.methodGetPostDeleteFilter()
+ *
+ * ### With `null` RequestMethodFilter no api call will be restricted.
+ *
+ * @property editTextIdsToIgnore List of `EditText` resource identifiers that are to be ignored and
+ * no text input performed during auto(unscripted) test. A fully qualified resource identifier in the form of "package:type/entry"
+ * has to be provided.
+ *
+ * @property defaultEditTextIdValueMap a map of resource id to `default value`s for `EditText` in auto test mode.
+ * `String` key has to be fully qualified resource identifier in the form of "package:type/entry" for `Edittext` and for map `value`,
+ * value of intended input of `EditText`.
+ *
+ * @property defaultEditTextHintValueMap a map of `hint text` to `default value`s for `EditText` in auto test mode. For value input in
+ * `EditText`, "resource id" matching will have higher priority than "hint". First "resource id" will be checked for map, if failed then "hint"
+ * text will be checked.
+ *
+ * * */
 @Keep
 data class TestSettings(
     val testScriptPath:String?=null,
