b607e3a8d129fe85280a9a6cbb184f1ef27817ad
Bikash Das <das.bikash.dev@gmail.com>
2021-07-15 05:35:09 +0600

    'RoboActionType' added.

---
+++ app_crawler_model/src/main/java/com/dasBikash/app_crawler_model/RoboActionType.kt
@@ -0,0 +1,8 @@
+package com.dasBikash.app_crawler_model
+
+/**
+ * Each enum type corresponds to supported robo action types
+ * */
+enum class RoboActionType{
+    VIEW_CLICKED,LIST_ITEM_CLICKED,VIEW_TEXT_CHANGED,PRESSED_BACK
+}
