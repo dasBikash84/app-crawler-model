d56c8d8e0a336a51a94d5dab176229608b5501fb
Bikash Das <das.bikash.dev@gmail.com>
2021-07-17 03:27:06 +0600

    'RoboActionType' removed.

--- app_crawler_model/src/main/java/com/dasBikash/app_crawler_model/RoboActionType.kt
+++
@@ -1,8 +0,0 @@
-package com.dasBikash.app_crawler_model
-
-/**
- * Each enum type corresponds to supported robo action types
- * */
-enum class RoboActionType{
-    VIEW_CLICKED,LIST_ITEM_CLICKED,VIEW_TEXT_CHANGED,PRESSED_BACK
-}
